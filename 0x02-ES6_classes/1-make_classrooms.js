bda42ae23483de468c1fd2301c72e6595469dd62
Dymond <123193636+Ufoma@users.noreply.github.com>
2024-10-04 05:06:36 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -6,16 +6,9 @@ import ClassRoom from "./0-classroom.js";
  * Initializes an array of ClassRoom objects.
  * @returns {ClassRoom[]} Array of ClassRoom objects.
  */
-function initializeRooms() {
+export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
-}
-
-const rooms = initializeRooms();
-
-rooms.forEach((room, index) => {
-  console.log(`Room ${index + 1} size: ${room._maxStudentsSize}`);
-});
